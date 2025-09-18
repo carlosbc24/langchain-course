e2bcfacb856b0bb9f8972832b6c560308e14b8d4
CARLOS BREUER CARRASCO <cbc@unex.es>
2025-09-19 00:21:16 +0200

    add search non cost agent

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ from langchain.agents.react.agent import create_react_agent
 from langchain_core.output_parsers.pydantic import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableLambda
-from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
 from langchain_tavily import TavilySearch
 
 from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
 from schemas import AgentResponse
 
 tools = [TavilySearch()]
-llm = ChatOpenAI(model="gpt-5")
+llm = ChatOllama(model="llama3.2")
 react_prompt = hub.pull("hwchase17/react")
 output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
 react_prompt_with_format_instructions = PromptTemplate(
